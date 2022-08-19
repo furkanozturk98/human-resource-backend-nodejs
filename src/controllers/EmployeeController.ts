b57a26620260c503ca66caee2bdcba9873d62954
Furkan <furkan.ozturk.98@hotmail.com>
2022-08-19 21:51:59 +0300

    refactor: simplify method and parameter names

--- src/controllers/EmployeeController.ts
+++ src/controllers/EmployeeController.ts
@@ -1,37 +1,62 @@
 import * as express from 'express';
 import EmployeesService from '../services/EmployeeService';
-import * as argon2 from 'argon2';
-import debug from 'debug';
+// import * as argon2 from 'argon2';
 
-const log: debug.IDebugger = debug('app:employees-controller');
-class EmployeeController {
-
-    async list(req: express.Request, res: express.Response) {
+class EmployeeController
+{
+    /**
+     * @param req
+     * @param res
+     */
+    async list(req: express.Request, res: express.Response): Promise<void>
+    {
         const employees = await EmployeesService.list(15, 1);
 
         res.status(200).send(employees);
     }
 
-    async getEmployeeById(req: express.Request, res: express.Response) {
-        const employee = await EmployeesService.show(req.params.employeeId);
+    /**
+     * @param req
+     * @param res
+     */
+    async show(req: express.Request, res: express.Response): Promise<void>
+    {
+        const employee = await EmployeesService.show(req.params.id);
+
         res.status(200).send(employee);
     }
 
-    async creatEemployee(req: express.Request, res: express.Response) {
+    /**
+     * @param req
+     * @param res
+     */
+    async create(req: express.Request, res: express.Response): Promise<void>
+    {
         // req.body.password = await argon2.hash(req.body.password);
         const employee = await EmployeesService.create(req.body);
-        res.status(201).send();
+
+        res.status(201).send(employee);
     }
 
-    async put(req: express.Request, res: express.Response) {
-        // req.body.password = await argon2.hash(req.body.password);
-        const data = await EmployeesService.update(req.params.employeeId, req.body);
-        log(data);
+    /**
+     * @param req
+     * @param res
+     */
+    async update(req: express.Request, res: express.Response): Promise<void>
+    {
+        const data = await EmployeesService.update(req.params.id, req.body);
+
         res.status(204).send(data);
     }
 
-    async removEemployee(req: express.Request, res: express.Response) {
-        log(await EmployeesService.delete(req.params.employeeId));
+    /**
+     * @param req
+     * @param res
+     */
+    async delete(req: express.Request, res: express.Response): Promise<void>
+    {
+        await EmployeesService.delete(req.params.id);
+
         res.status(204).send(``);
     }
 }
--- src/routes/EmployeeRoutes.ts
+++ src/routes/EmployeeRoutes.ts
@@ -1,32 +1,33 @@
-import {CommonRoutesConfig} from '../common/routes.config';
+import {Routes} from '../common/Routes';
 import EmployeeController from '../controllers/EmployeeController';
 import EmployeeMiddleware from '../middlewares/EmployeeMiddleware';
 import * as express from 'express';
 
-class EmployeesRoutes extends CommonRoutesConfig {
+class EmployeesRoutes extends Routes {
     constructor(app: express.Application) {
         super(app, 'EmployeesRoutes');
     }
 
-    configureRoutes() {
+    configureRoutes(): express.Application
+    {
         this.app.route(`/employees`)
             .get(EmployeeController.list)
             .post(
                 EmployeeMiddleware.validateRequiredemployeeBodyFields,
                 EmployeeMiddleware.validateSameEmailDoesntExist,
-                EmployeeController.creatEemployee);
+                EmployeeController.create);
 
-        this.app.param(`employeeId`, EmployeeMiddleware.extractemployeeId);
+        this.app.param(`id`, EmployeeMiddleware.extractId);
 
-        this.app.route(`/employees/:employeeId`)
+        this.app.route(`/employees/:id`)
             .all(EmployeeMiddleware.validateemployeeExists)
-            .get(EmployeeController.getEmployeeById)
-            .delete(EmployeeController.removEemployee);
+            .get(EmployeeController.show)
+            .delete(EmployeeController.delete);
 
-        this.app.put(`/employees/:employeeId`,[
+        this.app.put(`/employees/:id`,[
             EmployeeMiddleware.validateRequiredemployeeBodyFields,
             EmployeeMiddleware.validateSameEmailBelongToSameemployee,
-            EmployeeController.put
+            EmployeeController.update
         ]);
 
         return this.app;
